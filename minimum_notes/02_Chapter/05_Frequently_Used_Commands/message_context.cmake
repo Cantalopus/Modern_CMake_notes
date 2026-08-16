05f49e3b6321bc8ce697e8867546ea41f0b060a0
WR Allbritain <wes.allbritain@gmail.com>
2026-08-16 16:17:47 -0500

    conclude: Chapter 2

--- minimum_notes/02_Chapter/05_Frequently_Used_Commands/message_context.cmake
+++ minimum_notes/02_Chapter/05_Frequently_Used_Commands/message_context.cmake
@@ -1,3 +1,4 @@
+#[[
 function(foo)
   list(APPEND CMAKE_MESSAGE_CONTEXT "foo")
   message("foo message")
@@ -14,3 +15,35 @@ list(APPEND CMAKE_MESSAGE_INDENT " ")
 message("Befor `foo`")
 foo()
 message("After `foo`")
+
+message("---------------")
+]]
+
+# Run with: cmake -P message_context.cmake --log-context --log-level=<DEBUG,VERBOSE,TRACE>
+
+function(bar)
+  list(APPEND CMAKE_MESSAGE_CONTEXT "bar")
+  message(VERBOSE "bar VERBOSE message")
+endfunction()
+
+function(baz)
+  list(APPEND CMAKE_MESSAGE_CONTEXT "baz")
+  message(DEBUG "baz DEBUG message")
+endfunction()
+
+function(foo_1)
+  list(APPEND CMAKE_MESSAGE_CONTEXT "foo_1")
+  bar()
+  message(TRACE "foo_1 TRACE message")
+  baz()
+endfunction()
+
+list(APPEND CMAKE_MESSAGE_CONTEXT "top_1")
+
+message(VERBOSE "Before `foo`")
+foo_1()
+message(VERBOSE "After `foo`")
+
+list(POP_BACK CMAKE_MESSAGE_CONTEXT)
+message("done... ")
+  
